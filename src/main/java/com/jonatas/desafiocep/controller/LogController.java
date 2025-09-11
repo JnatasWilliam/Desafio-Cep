16997aac38dc89e5c781b5e4ea26b67638912d97
JnatasWilliam <jonatas.santos80@yahoo.com>
2025-09-11 00:46:22 -0300

    Implementando o log

---
+++ src/main/java/com/jonatas/desafiocep/controller/LogController.java
@@ -0,0 +1,39 @@
+package com.jonatas.desafiocep.controller;
+
+import com.jonatas.desafiocep.model.ConsultaLog;
+import com.jonatas.desafiocep.repository.ConsultaLogRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/logs")
+public class LogController {
+
+    private final ConsultaLogRepository repo;
+
+    public LogController(ConsultaLogRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping
+    public Page<ConsultaLog> listarTodos(Pageable pageable) {
+        return repo.findAll(pageable);
+    }
+
+    @GetMapping(params = "status")
+    public Page<ConsultaLog> filtrarPorStatus(
+            @RequestParam String status,
+            Pageable pageable
+    ) {
+        return repo.findByStatusConsulta(status, pageable);
+    }
+
+    @GetMapping(params = "usuarioId")
+    public Page<ConsultaLog> filtrarPorUsuario(
+            @RequestParam String usuarioId,
+            Pageable pageable
+    ) {
+        return repo.findByUsuarioId(usuarioId, pageable);
+    }
+}
--- src/main/java/com/jonatas/desafiocep/repository/ConsultaLogRepository.java
+++ src/main/java/com/jonatas/desafiocep/repository/ConsultaLogRepository.java
@@ -1,7 +1,15 @@
 package com.jonatas.desafiocep.repository;
 
 import com.jonatas.desafiocep.model.ConsultaLog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ConsultaLogRepository extends MongoRepository<ConsultaLog, String> {
+    Page<ConsultaLog> findAll(Pageable pageable);
+
+    Page<ConsultaLog> findByStatusConsulta(String statusConsulta, Pageable pageable);
+
+    Page<ConsultaLog> findByUsuarioId(String usuarioId, Pageable pageable);
+
 }
