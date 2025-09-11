65d74d3d81d00c718fcf3782c9c6428c4a4e32f8
JnatasWilliam <jonatas.santos80@yahoo.com>
2025-09-11 20:11:15 -0300

    Criando Usuario Controller

---
+++ src/main/java/com/jonatas/desafiocep/controller/UsuarioController.java
@@ -0,0 +1,34 @@
+package com.jonatas.desafiocep.controller;
+
+import com.jonatas.desafiocep.model.Usuario;
+import com.jonatas.desafiocep.repository.UsuarioRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final UsuarioRepository usuarioRepo;
+
+    public UsuarioController(UsuarioRepository usuarioRepo) {
+        this.usuarioRepo = usuarioRepo;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getUsuarioAutenticado() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        Usuario usuario = usuarioRepo.findByUsername(username)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado"));
+
+        return ResponseEntity.ok(usuario);
+    }
+}
